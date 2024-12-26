5dca6b27117016b350c5a1cf4fd7068f25691c5e
cmuciscaguinea <s.aguinea.joemar@cmu.edu.ph>
2024-12-26 12:53:20 +0800

    first commit

---
+++ java/exportkit/figma/frame_1_activity.java
@@ -0,0 +1,65 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		frame_1
+	 *	@date 		Tuesday 19th of December 2023 04:47:48 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.TextView;
+
+public class frame_1_activity extends Activity {
+
+	
+	private View _bg__frame_1;
+	private View _bg__profile_ek1;
+	private View rectangle_4;
+	private TextView profile_ek2;
+	private View _bg__my_items_ek1;
+	private View rectangle_4_ek1;
+	private TextView my_items_ek2;
+	private View _bg__home_button_ek1;
+	private View rectangle_4_ek2;
+	private TextView home;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.frame_1);
+
+		
+		_bg__frame_1 = (View) findViewById(R.id._bg__frame_1);
+		_bg__profile_ek1 = (View) findViewById(R.id._bg__profile_ek1);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		profile_ek2 = (TextView) findViewById(R.id.profile_ek2);
+		_bg__my_items_ek1 = (View) findViewById(R.id._bg__my_items_ek1);
+		rectangle_4_ek1 = (View) findViewById(R.id.rectangle_4_ek1);
+		my_items_ek2 = (TextView) findViewById(R.id.my_items_ek2);
+		_bg__home_button_ek1 = (View) findViewById(R.id._bg__home_button_ek1);
+		rectangle_4_ek2 = (View) findViewById(R.id.rectangle_4_ek2);
+		home = (TextView) findViewById(R.id.home);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
